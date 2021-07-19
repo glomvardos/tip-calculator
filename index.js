3d95dab0c4ca3f9c9bdeeaf7b2e9c90f37d51981
glomvardos <gerapes90@yahoo.gr>
2021-07-19 21:03:05 +0300

    total and tip amount calculations

--- index.js
+++ index.js
@@ -15,30 +15,124 @@ const tipButtons = document.querySelectorAll('.btn')
 const billRequiredMsg = document.getElementById('msg-bill')
 const peopleRequiredMsg = document.getElementById('msg-people')
 
+// Initial values
+const init = () => {
+  reset.setAttribute('disabled', 'true')
+  totalAmount.textContent = '0.00€'
+  tipAmount.textContent = '0.00€'
+  billInput.value = ''
+  peopleInput.value = ''
+  customInput.value = ''
+}
+
+// Get the total and tip amount for each person
+const getTotalAndTipAmount = (amount, event, custom = false) => {
+  let amountPerPerson
+  if (amount === totalAmount) {
+    amountPerPerson = +billInput.value / +peopleInput.value
+  }
+  if (amount === tipAmount) {
+    const tipPercentage = event && event.target.textContent.replace('%', '') / 100
+    amountPerPerson = custom
+      ? +customInput.value / +peopleInput.value
+      : (+billInput.value * tipPercentage) / +peopleInput.value
+  }
+
+  amount.textContent = amountPerPerson.toFixed(2) + '€'
+}
+
+// Show error for invalid input
 const showError = input => {
   input.classList.remove('hideError')
   input.classList.add('showError')
 }
 
-const hideError = () => {
-  billRequiredMsg.classList.remove('showError')
-  billRequiredMsg.classList.add('hideError')
-  peopleRequiredMsg.classList.remove('showError')
-  peopleRequiredMsg.classList.add('hideError')
+// Hide error for invalid input
+const hideError = input => {
+  input.classList.remove('showError')
+  input.classList.add('hideError')
 }
 
-tipButtons.forEach(btn =>
-  btn.addEventListener('click', event => {
-    if (
-      billInput.value === '' ||
-      billInput.value <= '0' ||
-      peopleInput.value === '' ||
-      peopleInput.value <= '0'
-    ) {
-      showError(billRequiredMsg)
-      showError(peopleRequiredMsg)
-      return
-    }
-    hideError()
+// Reset the class for each button
+const buttonClassHandler = () => {
+  tipButtons.forEach(btn => {
+    btn.classList.remove('active')
+    btn.classList.add('main__tip-btn')
   })
-)
+}
+
+// Only numbers are accepted as values
+const regexBill = /^(0*[1-9][0-9]*(\.[0-9]+)?|0+\.[0-9]*[1-9][0-9]*)$/
+const regexPeople = /^[1-9][0-9]*$/
+
+// Check if the input is a number
+const isValidNum = (input, regex) => input.match(regex)
+
+// Check if all inputs are valid to continue
+const formValidationHandler = () =>
+  isValidNum(billInput.value, regexBill) && isValidNum(peopleInput.value, regexPeople)
+    ? true
+    : false
+
+// Check each input field if is valid
+const inputValidationHandler = () => {
+  if (!isValidNum(billInput.value, regexBill)) showError(billRequiredMsg)
+  if (!isValidNum(peopleInput.value, regexPeople)) showError(peopleRequiredMsg)
+
+  if (isValidNum(billInput.value, regexBill)) hideError(billRequiredMsg)
+  if (isValidNum(peopleInput.value, regexPeople)) hideError(peopleRequiredMsg)
+}
+
+// Calculation Handler
+const tipCalculationHandler = event => {
+  event.preventDefault()
+
+  buttonClassHandler()
+  inputValidationHandler()
+
+  const isValid = formValidationHandler()
+
+  if (isValid) {
+    reset.removeAttribute('disabled')
+
+    hideError(billRequiredMsg)
+    hideError(peopleRequiredMsg)
+
+    // Add class for the active button
+    event.target.classList.remove('main__tip-btn')
+    event.target.classList.add('active')
+
+    getTotalAndTipAmount(totalAmount)
+    getTotalAndTipAmount(tipAmount, event)
+  }
+}
+
+const customCalculationHandler = event => {
+  tipButtons.forEach(btn => btn.removeEventListener('click', tipCalculationHandler, true))
+
+  inputValidationHandler()
+  const isValid = formValidationHandler()
+
+  if (event.key === 'Enter' && isValid) {
+    event.preventDefault()
+    reset.removeAttribute('disabled')
+    buttonClassHandler()
+
+    getTotalAndTipAmount(totalAmount)
+    getTotalAndTipAmount(tipAmount, undefined, true)
+
+    const getTipAmount = +customInput.value / +peopleInput.value
+    tipAmount.textContent = getTipAmount.toFixed(2) + '€'
+  }
+}
+
+// Buttons event listeners
+tipButtons.forEach(btn => btn.addEventListener('click', tipCalculationHandler))
+customInput.addEventListener('keypress', customCalculationHandler)
+
+// Reset calculator
+reset.addEventListener('click', event => {
+  event.preventDefault()
+  buttonClassHandler()
+  init()
+})
